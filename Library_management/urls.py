c90e1a5f4166fbc8768bc067377678777e7d5b4d
mj619 <Mayursingh>
2022-07-07 22:05:23 +0530

    Initial comment

---
+++ Library_management/urls.py
@@ -0,0 +1,51 @@
+"""Library_management URL Configuration
+
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+
+"""
+from django.conf.urls import include,url
+from django.contrib import admin
+from django.urls import path
+
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+from management import views
+from .feed import LatestEntriesFeed
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+
+    path('books/', views.BookListView, name='books'),
+    path('book/<int:pk>', views.BookDetailView, name='book-detail'),
+    path('book/create/', views.BookCreate, name='book_create'),
+    path('book/<int:pk>/update/', views.BookUpdate, name='book_update'),
+    path('book/<int:pk>/delete/', views.BookDelete, name='book_delete'),
+
+    path('student/<int:pk>/delete/', views.StudentDelete, name='student_delete'),
+    path('student/create/', views.StudentCreate, name='student_create'),
+    path('student<int:pk>/update/', views.StudentUpdate, name='student_update'),
+    path('student/<int:pk>', views.StudentDetail, name='student_detail'),
+    path('student/', views.StudentList, name='student_list'),
+    path('student/book_list', views.student_BookListView, name='book_student'),
+    path('book/<int:pk>/request_issue/', views.student_request_issue, name='request_issue'),
+
+    path('feed/', LatestEntriesFeed(), name='feed'),
+    path('return/<int:pk>', views.ret, name='ret'),
+    path('rating/<int:pk>/update/', views.RatingUpdate, name='rating_update'),
+    path('rating/<int:pk>/delete/', views.RatingDelete, name='rating_delete'),
+
+
+url(r'^search_b/', views.search_book, name="search_b"),
+url(r'^search_s/', views.search_student, name="search_s")
+]
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
